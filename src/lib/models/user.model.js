f4f0309fee8db26b3577b1a5c4cfabe5d6601bd8
alohasally <peacheeks@gmail.com>
2024-09-17 21:59:32 +0900

    Add mongoDB and mongoose to save user data

---
+++ src/lib/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mogoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
---
+++ src/lib/mongodb/mogoose.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (initialized) {
+    console.log("MongoDb already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "next auth app",
+      useNewUrlparser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongodb connected");
+    initialized = true;
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+  }
+};
